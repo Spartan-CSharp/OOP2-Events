6ee0c4a4053df96a170b10f5365613aee9017b2b
Pierre Plourde <pierre@spartancsharp.net>
2024-02-09 10:33:12 -0500

    Create Code
    Create an event in a class and then consume that class. Attach a listener to the event and have it write to the console.

---
+++ Events/PersonModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+	public class PersonModel
+	{
+		public event EventHandler<string> LastNameChanged;
+
+		private string _firstName;
+
+		public string FirstName
+		{
+			get
+			{
+				return _firstName;
+			}
+			set
+			{
+				_firstName = value;
+			}
+		}
+
+		private string _lastName;
+
+		public string LastName
+		{
+			get
+			{
+				return _lastName;
+			}
+			set
+			{
+				LastNameChanged?.Invoke(this, $"Last Name has been changed from {_lastName} to {value}.");
+				_lastName = value;
+			}
+		}
+
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+		public PersonModel(string firstName, string lastName)
+		{
+			_firstName = firstName;
+			_lastName = lastName;
+		}
+	}
+}
---
+++ Events/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			PersonModel person = new PersonModel("Tim", "Corey");
+			person.LastNameChanged += Person_LastNameChanged;
+
+			Console.WriteLine(person.FullName);
+
+			person.LastName = "Storm";
+
+			Console.WriteLine(person.FullName);
+
+			_ = Console.ReadLine();
+		}
+
+		private static void Person_LastNameChanged(object sender, string e)
+		{
+			PersonModel model = (PersonModel)sender;
+
+			Console.WriteLine();
+			Console.WriteLine($"{model.FullName}'s last name has changed: {e}");
+			Console.WriteLine();
+		}
+	}
+}
